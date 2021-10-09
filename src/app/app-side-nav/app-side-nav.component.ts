a86ce0b918589e7b3a148cee5cc3f6f3bbdf0d77
rajthapa.dev <rajthapa.dev@outlook.com>
2021-10-09 23:27:29 +1000

    added navigation links

--- src/app/app-side-nav/app-side-nav.component.ts
+++ src/app/app-side-nav/app-side-nav.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './app-side-nav.component.html',
-  styleUrls: ['./app-side-nav.component.css']
+  styleUrls: ['./app-side-nav.component.css'],
 })
 export class AppSideNavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  ngOnInit(): void {}
 }
